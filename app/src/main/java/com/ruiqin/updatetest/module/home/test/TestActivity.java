3780dfe919999b1cf2537448e1ffc707476034b1
shenruiqin <ruiqin.shen@100cb.cn>
2017-07-21 18:34:42 +0800

    优化

---
+++ app/src/main/java/com/ruiqin/updatetest/module/home/test/TestActivity.java
@@ -0,0 +1,15 @@
+package com.ruiqin.updatetest.module.home.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ruiqin.updatetest.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
